6dbcfedaa57bf275bda74f546906e5c6fe5022c9
sadamvullah.i <123800909+Sadamvullah@users.noreply.github.com>
2023-03-25 15:37:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          solo-_-hacker
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i>SADAMVULLAH.I</i>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
